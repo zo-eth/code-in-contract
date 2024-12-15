98175d047a90985992702500c85ca74302f23150
Zo <76999066+zo-eth@users.noreply.github.com>
2024-12-15 15:36:13 +0900

    change the name

--- codein.rs
+++ codein.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("-----------");
+declare_id!("J4zN6m7TdRRc75Xp2XuR3wjjVUHNB1VMaMQnk3cyqJaH");
 
 #[program]
 pub mod code_in {
@@ -18,16 +18,16 @@ pub mod code_in {
         );
 
         require_keys_eq!(
-            ctx.accounts.message_account.key(),
+            ctx.accounts.code_account.key(),
             expected_pda,
             ErrorCode::InvalidAccount
         );
 
-        let message_account = &mut ctx.accounts.message_account;
-        message_account.bump = expected_bump;
-        message_account.message = String::new();
-        message_account.method = 0;
-        message_account.decode_break = 0;
+        let code_account = &mut ctx.accounts.code_account;
+        code_account.bump = expected_bump;
+        code_account.code = String::new();
+        code_account.method = 0;
+        code_account.decode_break = 0;
 
         let db_account = &mut ctx.accounts.db_account;
         db_account.bump = expected_db_bump;
@@ -39,9 +39,9 @@ pub mod code_in {
         Ok(())
     }
 
-    pub fn send_message(
-        ctx: Context<SendMessage>,
-        message: String,
+    pub fn send_code(
+        ctx: Context<SendCode>,
+        code: String,
         before_tx: String,
         remaining_chunks: u32,
         method: u8,
@@ -52,7 +52,7 @@ pub mod code_in {
             &[b"yourseedhere", ctx.accounts.user.key.as_ref()],
             ctx.program_id,
         );
-        if ctx.accounts.message_account.key() != expected_pda {
+        if ctx.accounts.code_account.key() != expected_pda {
             return Err(ErrorCode::InvalidAccount.into());
         }
 
@@ -65,7 +65,7 @@ pub mod code_in {
             .iter()
             .find(|account| account.key == &expected_receiver);
         // PDA 잔액 확인
-        let pda_account_info = &ctx.accounts.message_account.to_account_info(); 
+        let pda_account_info = &ctx.accounts.code_account.to_account_info();
 
         let pda_balance = pda_account_info.lamports();
         if pda_balance < _required_lamports {
@@ -81,34 +81,34 @@ pub mod code_in {
             .expect("receiver_account not found")
             .try_borrow_mut_lamports()? += _required_lamports;
 
-        ctx.accounts.message_account.before_tx = before_tx;
-        ctx.accounts.message_account.message = message;
-        ctx.accounts.message_account.remaining_chunks = remaining_chunks - 1;
-        ctx.accounts.message_account.method = method;
-        ctx.accounts.message_account.decode_break = decode_break;
+        ctx.accounts.code_account.before_tx = before_tx;
+        ctx.accounts.code_account.code = code;
+        ctx.accounts.code_account.remaining_chunks = remaining_chunks - 1;
+        ctx.accounts.code_account.method = method;
+        ctx.accounts.code_account.decode_break = decode_break;
         Ok(())
     }
     pub fn send_chunk(
         ctx: Context<SendChunk>,
-        message: String,
+        code: String,
         before_tx: String,
         method: u8,
         decode_break: u8,
     ) -> Result<()> {
-        let chunk = ctx.accounts.message_account.remaining_chunks;
+        let chunk = ctx.accounts.code_account.remaining_chunks;
         if chunk > 0 {
             let (expected_pda, _expected_bump) = Pubkey::find_program_address(
                 &[b"seed here", ctx.accounts.user.key.as_ref()],
                 ctx.program_id,
             );
-            if ctx.accounts.message_account.key() != expected_pda {
+            if ctx.accounts.code_account.key() != expected_pda {
                 return Err(ErrorCode::InvalidAccount.into());
             }
-            ctx.accounts.message_account.remaining_chunks = chunk - 1;
-            ctx.accounts.message_account.before_tx = before_tx;
-            ctx.accounts.message_account.message = message;
-            ctx.accounts.message_account.method = method;
-            ctx.accounts.message_account.decode_break = decode_break;
+            ctx.accounts.code_account.remaining_chunks = chunk - 1;
+            ctx.accounts.code_account.before_tx = before_tx;
+            ctx.accounts.code_account.code = code;
+            ctx.accounts.code_account.method = method;
+            ctx.accounts.code_account.decode_break = decode_break;
             Ok(())
         } else {
             return Err(ErrorCode::ExceededChunk.into());
@@ -146,22 +146,22 @@ pub struct DBaccount {
 }
 
 #[account]
-pub struct MessageAccount {
+pub struct CodeAccount {
     pub bump: u8,
     pub decode_break: u8,
     pub method: u8,
     pub remaining_chunks: u32,
-    pub message: String,
+    pub code: String,
     pub before_tx: String,
 }
 
 #[derive(Accounts)]
 pub struct UserInitialize<'info> {
     #[account(mut)]
-    pub user: Signer<'info>, 
+    pub user: Signer<'info>,
 
     #[account(init, payer = user,seeds = [b"seedhere", user.key().as_ref()],bump,space = 1+1+1+4+850+100)]
-    pub message_account: Account<'info, MessageAccount>,
+    pub code_account: Account<'info, CodeAccount>,
 
     #[account(init, payer = user,seeds = [b"seed here", user.key().as_ref()],bump,space =1+20+100+10+50)]
     pub db_account: Account<'info, DBaccount>,
@@ -169,34 +169,34 @@ pub struct UserInitialize<'info> {
 }
 
 #[derive(Accounts)]
-pub struct SendMessage<'info> {
+pub struct SendCode<'info> {
     #[account(mut)]
-    pub user: Signer<'info>, 
+    pub user: Signer<'info>,
     #[account(mut)]
-    pub message_account: Account<'info, MessageAccount>, 
-    pub system_program: Program<'info, System>, 
+    pub code_account: Account<'info, CodeAccount>,
+    pub system_program: Program<'info, System>,
 }
 #[derive(Accounts)]
 pub struct SendChunk<'info> {
     #[account(mut)]
-    pub user: Signer<'info>, 
+    pub user: Signer<'info>,
     #[account(mut)]
-    pub message_account: Account<'info, MessageAccount>, 
-    pub system_program: Program<'info, System>, 
+    pub code_account: Account<'info, CodeAccount>,
+    pub system_program: Program<'info, System>,
 }
 #[derive(Accounts)]
 pub struct DbCodeIn<'info> {
     #[account(mut)]
-    pub user: Signer<'info>, 
+    pub user: Signer<'info>,
     #[account(mut)]
-    pub db_account: Account<'info, DBaccount>, 
-    pub system_program: Program<'info, System>, 
+    pub db_account: Account<'info, DBaccount>,
+    pub system_program: Program<'info, System>,
 }
 
 // 에러 코드 정의
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Insufficient funds to send message.")]
+    #[msg("Insufficient funds to send code.")]
     InsufficientFunds,
     #[msg("Invalid wallet address.")]
     InvalidWallet,
@@ -204,10 +204,10 @@ pub enum ErrorCode {
     InvalidReceiver,
     #[msg("Funds were not received by the expected wallet.")]
     FundsNotReceived,
-    #[msg("Provided message account is invalid.")]
+    #[msg("Provided code account is invalid.")]
     InvalidAccount,
-    #[msg("InvalidMessageFormat")]
-    InvalidMessageFormat,
+    #[msg("InvalidCodeFormat")]
+    InvalidCodeFormat,
     #[msg("InvalidInstructionData")]
     InvalidInstructionData,
     #[msg("InvalidTransfer")]
